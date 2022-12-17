6dd3bdb3d752ca5abff5c2c8dbb615f48f8d9337
Muhammad Haseeb Ali <haseeb.aha786@gmail.com>
2022-12-17 11:07:50 +0500

    ...

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require("cors")
+var CORS = require("cors")
 
 var indexRouter = require('./routes/index');
 var proposalRouter = require('./routes/proposal');
@@ -10,12 +10,12 @@ var proposalRouter = require('./routes/proposal');
 var app = express(),
     port = process.env.PORT || "3000"
 
-    
-app.use(cors)
+app.use(CORS);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/proposal', proposalRouter);
